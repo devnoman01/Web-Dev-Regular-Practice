bca6a8dd33b5404759292510713f09b4e49ce7fb
devnoman01 <devnoman01@gmail.com>
2022-02-02 04:38:57 +0600

    Module-17 file 17-8

---
+++ 17 Module/17-8.js
@@ -0,0 +1,22 @@
+var tShirtPrice = 200;
+console.log(typeof (tShirtPrice));
+
+var tShirtPrices2 = 300;
+console.log(typeof (tShirtPrice));
+
+var isRaining = true;
+console.log(typeof (isRaining));
+
+var isRomantic = true;
+console.log(typeof (isRomantic));
+
+var today = new Date();
+console.log(typeof (today));
+
+//  special edition
+var firstNumber = 0.1;
+var secondNumber = 0.2;
+var total = firstNumber + secondNumber;
+total.toFixed(3);
+total = parseFloat(total);
+console.log(total)
