19da2dc0511ecf931ad989ec642cad838cceec7f
devnoman01 <devnoman01@gmail.com>
2022-02-02 04:44:13 +0600

    Module-17 file 17-9

---
+++ 17 Module/17-9.js
@@ -0,0 +1,16 @@
+var eggPrice = 39;
+eggPrice = 39 * 2;
+console.log(eggPrice);
+console.log(typeof eggPrice);
+
+//  string
+var singer = 'Tahsan';
+
+//  boolean
+var isCold = true;
+
+//  modulus
+var mangoes = 7;
+var hungryPerson = 2;
+var remaining = mangoes %  hungryPerson;
+console.log(remaining);
