111a56c414995513c7c4bc3f9664eb02cc52cddf
devnoman01 <devnoman01@gmail.com>
2022-02-02 04:16:32 +0600

    Module-17 file 17-6

---
+++ 17 Module/17-6.js
@@ -0,0 +1,20 @@
+var price1 = 31;
+var price2 = 10;
+var sum = price1 + price2;
+var difference = price2 - price1;
+var multiplication = price1 * price2;
+var division = price1 / price2;
+
+console.log(sum);
+console.log(difference);
+console.log(multiplication);
+console.log(division);
+
+var age = 14;
+age += 1;
+age++;
+console.log(age);
+
+var love = 100;
+vove = love - 12;
+console.log(love)
