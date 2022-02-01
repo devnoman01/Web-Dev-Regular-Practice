38756e0741409d88f172e2da522977fcda2b0e06
devnoman01 <devnoman01@gmail.com>
2022-02-02 03:43:56 +0600

    Module-17 file 17-4

---
+++ 17 Module/17-4.js
@@ -0,0 +1,16 @@
+var name = "Mr. Abul";
+
+//  reserved keyworkd now allowed as variable
+var true = 12;
+var false = 'subscribed';
+
+//  name should be one word
+var my name = 'Mr. Abdullah';
+
+//  quote is not allowed
+var "name" = "James Bond";
+
+var hero-name = "James Bond";
+var hero_name = "James Bond";       //  underscore allowed
+var heroName = "James Bond";        //  camelCase is recommended
+var heroname = "James Bond";        //  useable but camelCase recommended
