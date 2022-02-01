4f20b08dff74e18a451399afc9ab5088e36f8eae
devnoman01 <devnoman01@gmail.com>
2022-02-02 03:58:32 +0600

    Module-17 file 17-5

---
+++ 17 Module/17-5.js
@@ -0,0 +1,20 @@
+var onionPrice = 43;
+console.log(onionPrice);
+
+var eggQuantity = 7;
+var eggPrice = 35;
+console.log(eggPrice);
+
+var eggPriceMultiplication = eggPrice * eggQuantity;
+console.log(eggPriceMultiplication);
+
+var totalPrice = onionPrice + eggPrice;
+console.log(totalPrice);
+
+var priceDifference = onionPrice - eggPrice;
+console.log(priceDifference);
+
+var moneyAvailable = 100;
+var orangePrice = 20;
+var orangeQuantity = moneyAvailable / orangePrice;
+console.log(orangeQuantity);
