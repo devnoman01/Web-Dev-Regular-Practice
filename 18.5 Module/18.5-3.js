dc889e25418f426d8a41cc62db3b9e3165954d49
devnoman01 <devnoman01@gmail.com>
2022-02-03 02:51:00 +0600

    Module-18.5 file 18.5-3

---
+++ 18.5 Module/18.5-3.js
@@ -0,0 +1,20 @@
+var number = 0;
+
+while(number <= 10){
+    console.log(number);
+    number++;
+}
+
+//  print odd number from 1 to 20
+var a = 1;
+while(a <= 20){
+    console.log(a);
+    a += 2;
+}
+
+//  print even number from 0 to 20
+var b = 0;
+while(b <= 20){
+    console.log(b);
+    b += 2;
+}
