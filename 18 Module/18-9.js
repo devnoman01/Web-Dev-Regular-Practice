4f4bd96571e536da41a2ffa070bcd1956c19c24a
devnoman01 <devnoman01@gmail.com>
2022-02-02 21:12:59 +0600

    Module-18 file 18-9

---
+++ 18 Module/18-9.js
@@ -0,0 +1,42 @@
+//  practice problem-1
+
+var alia = 95;
+var dalia = 66;
+var salia = 80;
+var malia = 59;
+var lilia = 47;
+var jalaia = 77;
+
+if (mark < 50){
+    console.log('Grade F');
+}
+else if (mark >=50 && mark < 60){
+    console.log('Grade D');
+}
+else if (mark >=60 && mark < 70){
+    console.log('Grade C');
+}
+else if (mark >=70 && mark < 80){
+    console.log('Grade B');
+}
+else if (mark >=80 && mark < 90){
+    console.log('Grade A');
+}
+else if (mark >= 90){
+    console.log('Grade A+');
+}
+
+
+//  practice problem-2
+if (signal == 'red'){
+    console.log('Stop. Do not cross road now.');
+}
+else if (signal == 'yellow'){
+    console.log('Get ready. You will be able to cross road shortly.');
+}
+else if (signal == 'green'){
+    console.log('You can cross road.');
+}
+else{
+    console.log('Signal is not working properly');
+}
