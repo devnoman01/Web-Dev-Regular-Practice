aa80df5ea9abb92a647757dc3fcd7074091998c9
devnoman01 <devnoman01@gmail.com>
2022-02-03 03:38:44 +0600

    Module-18.5 file 18.5-5

---
+++ 18.5 Module/18.5-5.js
@@ -0,0 +1,15 @@
+var i = 0;
+while(i < 8){
+    console.log(i);
+    i++;
+}
+
+for (let i = 0; i < 7; i++) {
+    console.log(i);    
+}
+
+var numbers = [45, 73, 56, 43, 34, 59, 26, 95];
+for(let i = 0; i < numbers.length; i++){
+    var element = numbers[i];
+    console.log(element);
+}
