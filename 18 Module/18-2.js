b21d1d6db8fab9584656364466d1756233a0bebe
devnoman01 <devnoman01@gmail.com>
2022-02-02 09:57:40 +0600

    Module-18 file 18-2

---
+++ 18 Module/18-2.js
@@ -0,0 +1,8 @@
+var friendsAge = [11, 13, 15];
+console.log(friendsAge);
+
+var avengers = ["Iron man", "Captain America", "Thor", "Hulk"];
+console.log(avengers);
+
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+console.log(vowels);
