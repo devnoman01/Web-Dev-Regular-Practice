be4d7396679b74d66fc0461930999100970bd238
devnoman01 <devnoman01@gmail.com>
2022-02-02 19:06:30 +0600

    Module-18 file 18-5

---
+++ 18 Module/18-5.js
@@ -0,0 +1,18 @@
+console.log(5 > 6);         //  is 5 greater than 6
+console.log(5 < 6);         //  is 5 less than 6
+console.log(5 == 6);        //  is 5 equal to  6
+console.log(6 == 6);        //  is 6 equal to  6
+console.log(6 != 6);        //  is 6 not equal to  6
+
+var myMoney = 100;
+var yourMoney = 200;
+console.log(myMoney > yourMoney);
+console.log(myMoney < yourMoney);
+console.log(myMoney == yourMoney);
+console.log(myMoney !== yourMoney);
+
+//  and operator
+console.log(myMoney && yourMoney);
+
+//  or operator
+console.log(myMoney || yourMoney);
