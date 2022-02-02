81dd86983e2fba5b7fb3e68b40205d8c06a1a960
devnoman01 <devnoman01@gmail.com>
2022-02-03 05:51:26 +0600

    Module-19 file 19-4

---
+++ 19 Module/19-4.js
@@ -0,0 +1,35 @@
+//  add
+function addTwoNumber(num1, num2) {
+    var result = num1 + num2;
+    return result;
+}
+
+var addResult = addTwoNumber(50, 80);
+console.log(addResult);
+
+//  multiply
+function multiplyTwoNumber(num1, num2) {
+    var result = num1 * num2;
+    return result;
+}
+
+var multiplyResult = multiplyTwoNumber(5, 8);
+console.log(multiplyResult);
+
+//  subtract
+function subtractTwoNumber(num1, num2) {
+    var result = num1 + num2;
+    return result;
+}
+
+var subResult = subtractTwoNumber(80, 45);
+console.log(subResult);
+
+//  divide
+function divideTwoNumber(num1, num2) {
+    var result = num1 / num2;
+    return result;
+}
+
+var divideResult = divideTwoNumber(100, 25);
+console.log(divideResult);
