8b895ab7cbd5ef514c4571ef17a315ef50aac524
devnoman01 <devnoman01@gmail.com>
2022-02-02 09:42:15 +0600

    Module-18 file 18-1

---
+++ 18 Module/18-1.js
@@ -0,0 +1,15 @@
+//  number
+var watchPrice = 120;
+watchPrice = 150;
+
+//  integer: 1 2 3 4 5
+//  float: 2.33  3.1416
+
+
+//  string
+var userName = 'Captain Marvel';
+userName = 'Captain America';
+
+
+//  boolean
+var isExpensive = true;
