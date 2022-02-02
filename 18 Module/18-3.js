084c7a40ae8fccfaad916ca0ede21f70b0f60f11
devnoman01 <devnoman01@gmail.com>
2022-02-02 10:14:44 +0600

    Module-18 file 18-3

---
+++ 18 Module/18-3.js
@@ -0,0 +1,11 @@
+var teamMember = ['Rahim', 'Karim', 'Kalam', 'Salam', 'Balam'];
+var kalamIndex = teamMember.indexOf('Kalam');
+console.log(kalamIndex);
+
+var numbers = [23, 45, 93, 29, 81, 75];
+
+var findValue = numbers[1];
+console.log(findValue);
+
+var findIndex = numbers.indexOf(81);
+console.log(findIndex);
