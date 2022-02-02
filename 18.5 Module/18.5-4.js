a9c34433099b134a4826503f5784b9b07f21d22f
devnoman01 <devnoman01@gmail.com>
2022-02-03 03:24:14 +0600

    Module-18.5 file 18.5-4

---
+++ 18.5 Module/18.5-4.js
@@ -0,0 +1,14 @@
+//  print 1 to 10
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+// print even i from 0 to 10
+for (var i = 0; i < 10; i+=2) {
+    console.log(i);
+}
+
+// print odd i from 0 to 10
+for (var i = 1; i < 10; i+=2) {
+    console.log(i);
+}
