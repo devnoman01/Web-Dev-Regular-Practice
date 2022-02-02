8d2ba3e3541354c5ca686a00b5d5ad30bb639796
devnoman01 <devnoman01@gmail.com>
2022-02-02 19:17:22 +0600

    Module-18 file 18-6

---
+++ 18 Module/18-6.js
@@ -0,0 +1,14 @@
+if (isFoodReady == true){
+    console.log("Food is ready to serve.");
+}
+
+var iphonePrice = 113000;
+var budget = 10000;
+
+//  checking condition
+if (iphonePrice < budget){
+    console.log("Lets buy an iphone");
+}
+else{
+    console.log("Sorry, can't buy iphone");
+}
