04105f86ad99ca0cbda21277741065604a906ca5
devnoman01 <devnoman01@gmail.com>
2022-02-03 23:33:23 +0600

    Module-20 file 20-1

---
+++ 20 Module/20-1.js
@@ -0,0 +1,36 @@
+var time = '10:15pm';
+var bookPrice = 145;
+var isWhiteColor = false;
+
+//  array
+var partners = ['rafi', 'shafi', 'mafi', 'kafi'];
+var elementCount = partners.length;
+var mafiIndex = partners.indexOf('mafi');
+partners.push('nafi');
+partners.pop();
+
+//  condition
+if(bookPrice < 120){
+    console.log('I will buy this book');
+}
+else{
+    console.log('Price is bit high');
+}
+
+//  loop
+var i = 0;
+while(i <= 10){
+    i++;
+}
+
+for (let i = 0; i < array.length; i++) {
+    
+}
+
+//  function
+function checkDay(dayName) {
+    if(dayName == 'Friday' || dayName == 'Saturday'){
+        return true;
+    }
+}
+checkDay('Friday');
