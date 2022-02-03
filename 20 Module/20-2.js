9c6e6fe21c9c8557411cb233e8fb15a5835eb9c2
devnoman01 <devnoman01@gmail.com>
2022-02-03 23:40:33 +0600

    Module-20 file 20-2

---
+++ 20 Module/20-2.js
@@ -0,0 +1,15 @@
+//  inch to feet conversion
+function inchToFeet(inches) {
+    var feet = inches / 12;
+    return feet;
+}
+var inchesResult = inchToFeet(132);
+console.log(inchesResult);
+
+//  mile to kilometer conversion
+function milesToKm(miles) {
+    var km = miles * 1.609;
+    return km;
+}
+var kmResult = milesToKm(33);
+console.log(kmResult);
