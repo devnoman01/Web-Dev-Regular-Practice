8f57dfddd1e485843ca00777ac7460bd5650366a
devnoman01 <devnoman01@gmail.com>
2022-02-03 06:16:04 +0600

    Module-19 file 19-7

---
+++ 19 Module/19-7.js
@@ -0,0 +1,24 @@
+var color = 'orange';
+switch (color) {
+    case 'red':
+        console.log('color is red');
+        break;
+    case 'green':
+        console.log('color is green');
+        break;
+    case 'blue':
+        console.log('color is blue');
+        break;
+    case 'gray':
+        console.log('color is gray');
+        break;
+    case 'orange':
+        console.log('color is orange');
+        break;
+    case 'white':
+        console.log('color is white');
+        break;
+    default:
+        console.log('color is confusing');
+        break;
+}
