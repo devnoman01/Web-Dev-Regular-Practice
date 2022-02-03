f8038079f23c0f3f69c05b151809135b34ca7aa6
devnoman01 <devnoman01@gmail.com>
2022-02-04 03:57:55 +0600

    Module-20.5 file 20.5-1

--- 20.5 Module/20.5-1.js
+++ 20.5 Module/20.5-1.js
@@ -7,28 +7,3 @@ function celciusToFarenheit(temp) {
 var celValue = 30;
 var farValue = celciusToFarenheit(celValue);
 console.log('Farenheit value =', farValue);
-
-
-//  farenheit to celcius conversion
-function farenheitToCelcius(temp) {
-    let cel = (temp - 32) * 5/9;
-    return cel;
-}
-
-var farValue = 86;
-var celValue = farenheitToCelcius(farValue);
-console.log('Celcius value =', celValue);
-
-
-//  interest rate calculation
-//  per month 3000 for 10% for 5 year (60 months)
-function interestCalculate(payment, interest, months) {
-    let total = payment * Math.pow((1 + 0.1), 60);
-    return total;
-}
-
-var paymentPerMonth = 3000;
-var interestRate = 0.1;
-var months = 60;
-var totalAmount = interestCalculate(paymentPerMonth, interestRate, months);
-console.log('Total amount =',totalAmount.toFixed(2));
