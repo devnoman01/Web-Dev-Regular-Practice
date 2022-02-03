241abc8702b1c1aae71ac7f40bb5e3e632f5af2e
devnoman01 <devnoman01@gmail.com>
2022-02-03 06:00:41 +0600

    Module-19 file 19-5

---
+++ 19 Module/19-5.js
@@ -0,0 +1,24 @@
+var student = {
+    id: 13,
+    name: 'Abir',
+    marks: 80,
+    phone: 9299494
+};
+console.log(student);
+
+var smartphone = {
+    color: 'black',
+    price: 19999,
+    screenSize: 6,
+    storage: '12gb'
+};
+console.log(smartphone);
+
+var book = {
+    title: 'Start With Why',
+    author: 'Simon Sinek',
+    price: 200,
+    pages: 246,
+    publisher: 'penguin'
+};
+console.log(book);
