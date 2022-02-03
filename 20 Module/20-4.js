4adfb6fed8383f25d76c09445d4834fbec8a8793
devnoman01 <devnoman01@gmail.com>
2022-02-04 01:23:59 +0600

    Module-20 file 20-4

--- 20 Module/20-4.js
+++ 20 Module/20-4.js
@@ -1,6 +1,12 @@
 //  check leapyear
 function isLeapYear(year) {
-    if(year % 4 == 0){
+    if(year % 100 == 0){
+        console.log('Not Leap Year');
+    }
+    else if(year % 400 == 0){
+        console.log('Leap Year');
+    }
+    else if(year % 4 == 0){
         console.log('Leap Year');
     }
     else{
@@ -8,5 +14,5 @@ function isLeapYear(year) {
     }
 }
 
-const myYear = 2016;
+const myYear = 2000;
 var checkYear = isLeapYear(myYear);
