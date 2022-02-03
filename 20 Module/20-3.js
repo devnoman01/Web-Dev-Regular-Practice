6345d39468fb882d0b93b60a530f9f8938cc9662
devnoman01 <devnoman01@gmail.com>
2022-02-03 23:55:12 +0600

    Module-20 file 20-3

---
+++ 20 Module/20-3.js
@@ -0,0 +1,25 @@
+//  check even number
+function checkEven(value) {
+    if(value % 2 == 0){
+        console.log('Even number');
+    }
+    else{
+        console.log('Not even number')
+    }
+    
+}
+const number = 24;
+var result = checkEven(number);
+
+//  check odd number
+function checkOdd(value) {
+    if(value % 2 == 1){
+        console.log('Odd number');
+    }
+    else{
+        console.log('Not odd number')
+    }
+    
+}
+const number2 = 26;
+var result = checkOdd(number);
