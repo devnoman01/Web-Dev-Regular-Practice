7c4b5d855fcaad784574726621aa2b35050fd6e9
devnoman01 <devnoman01@gmail.com>
2022-02-04 16:29:20 +0600

    Module-22 file 22-4

---
+++ 22 Module/22-4.js
@@ -0,0 +1,18 @@
+function add(num1, num2) {
+    const sum = num1 + num2;
+    return sum;
+}
+
+var firstTotal = add(50, 30);
+console.log(firstTotal);
+
+
+function multiply(num1, num2) {
+    var result = num1 * num2;
+    return result;
+}
+
+var firstTotal = multiply(50, 30);
+console.log(firstTotal);
+
+
