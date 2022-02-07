d84172a29d4d0e0020cf7cc4cac9358f7e7a0bbc
devnoman01 <devnoman01@gmail.com>
2022-02-07 14:52:40 +0600

    Module-24 file 24-5

---
+++ 24 Module/24-5.js
@@ -0,0 +1,11 @@
+document.getElementById('second-blog').style.backgroundColor='lightyellow';
+
+const third = document.getElementById('third-blog');
+third.style.backgroundColor='lightgreen';
+third.style.color='blue';
+
+const thirdTitle = document.getElementById('third-title');
+thirdTitle.innerText='This is Blog-3';
+thirdTitle.style.fontSize='32px';
+thirdTitle.style.marginTop='10px';
+thirdTitle.style.marginBottom='10px';
