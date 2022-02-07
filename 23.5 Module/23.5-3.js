4b0067955f3ce5bb14cc36ef42ee248b1c5d2acd
devnoman01 <devnoman01@gmail.com>
2022-02-07 12:49:54 +0600

    Module-23.5 file 23.5-3

---
+++ 23.5 Module/23.5-3.js
@@ -0,0 +1,13 @@
+function factorial(i) {
+    if(i==1){
+        return 1;
+    }
+    return i*factorial(--i);
+}
+
+console.log(factorial(4));
+
+//  i * factorial(--i)
+//  4 * factorial(3)
+//  4 * 3 * factorial(2)
+//  4 * 3 * 2 * factorial(1)
