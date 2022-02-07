80023ae92e5d62e5efd2b92835b886504fcefcfc
devnoman01 <devnoman01@gmail.com>
2022-02-08 02:38:26 +0600

    Module-24 file 24-7

--- 24 Module/24-7.js
+++ 24 Module/24-7.js
@@ -6,5 +6,15 @@ const blogs = document.getElementsByClassName('blog');
 for(const blog of blogs){
     blog.setAttribute('style', 'color:blue;width:50%;marginTop=10px;marginBottom=10px;padding:1rem');
 }
-
 document.querySelectorAll('.blog');
+
+const secondTitle = document.getElementById('second-title');
+const secondTitleParent = secondTitle.parentNode;
+console.log(secondTitleParent);
+
+const parentOfParent = secondTitle.parentNode.parentNode;
+console.log(parentOfParent);
+
+const blog1Elements = document.getElementById('blog-1');
+const childOBlog1 = blog1Elements.childNodes;
+console.log(childOBlog1);
