1362a72828206ed74be6ea59fa636540004bd9c7
devnoman01 <devnoman01@gmail.com>
2022-02-07 13:55:22 +0600

    Module-24 file 24-1

---
+++ 24 Module/24-1.js
@@ -0,0 +1,7 @@
+/*
+
+JavaScript often abbreviated JS is a high-level, just in-time compiled language.
+
+JavaScript is a prototype-based, multi-paradigm, single-threaded, dynamic language, supporting object-oriented, imperative, and declarative (e.g. functional programming) styles
+
+*/
